af21c9f81c20946a6b1bb0e9ea9cd39e75f086e6
Vidhi Makani <vidhimakani007@gmail.com>
2023-02-27 14:31:13 +0530

    gradient add

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
-var body = document.getElementById("#gradient");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var color1 = document.querySelector("#color1");
+var color2 = document.querySelector("#color2");
 var h4= document.querySelector("h4");
 var random = document.querySelector("button");
 
+//set gradient color
 function setGradient(){
-    body.style.background="Linear-gradient(tto right, " + color1.value + ", " + color2.value + ")";
+    body.style.background="Linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+    h4.textContent=body.style.background;
 }
 
+//generate random color
+function randomColor(){
+    let maxVal = 0xFFFFFF; 
+    let randomNumber = Math.random() * maxVal; 
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);   
+    return `#${randColor.toUpperCase()}`;
+}
+
+//set random background
+function generateRandomColor(){
+    color1=randomColor();
+    color2=randomColor();
+
+    body.style.background = "Linear-gradient(to right, " + color1 + ", " + color2 + ")";
+    h4.textContent = body.style.background; 
+}
+
+
 color1.addEventListener("input",setGradient);
-color2.addEventListener("input",setGradient)
-random.addEventListener("click",generateRandomColor)
+color2.addEventListener("input",setGradient);
+random.addEventListener("click",generateRandomColor);
